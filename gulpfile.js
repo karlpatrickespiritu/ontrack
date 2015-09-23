c57e68405799ed75a6b5313f8719c4d9e353705b
karlpatrickespiritu <karl.espiritu@corp.mobile.co>
2015-09-24 05:33:15 +0800

    gulp file restructred

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,88 @@
 'use strict';
 
-var gulp 		= require('gulp'),
-	plumber		= require('gulp-plumber'),
-	concat		= require('gulp-concat'),
-	uglify		= require('gulp-uglify'),
-    watch		= require('gulp-watch'),
-	minifyCss	= require('gulp-minify-css');
+var gulp        = require('gulp'),
+    plumber     = require('gulp-plumber'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    watch       = require('gulp-watch'),
+    minifyCss   = require('gulp-minify-css');
 
 var config = {
-	dest: 'src',
+    dest: 'src',
 
-	scripts: [
-		// vendors
-		'./bower_components/html5-boilerplate/src/js/**/*.js',
-		'./bower_components/materialize/dist/js/materialize.min.js',
-		'./node_modules/mustache/mustache.min.js',
-		
-		// app
-		'./src/js/config/**/*.js',
-		'./src/js/controllers/**/*.js',
-		'./src/js/services/**/*.js',
-		'./src/js/pages/**/*.js'
-	],
+    scripts: [
+        // vendors
+        './bower_components/html5-boilerplate/src/js/**/*.js',
+        './bower_components/materialize/dist/js/materialize.min.js',
+        './node_modules/mustache/mustache.min.js',
 
-	css: [
-		// vendors
-		'./bower_components/normalize.css/normalize.css',
-		'./bower_components/materialize/dist/css/materialize.min.css'
-	],
+        // app
+        './src/js/config/**/*.js',
+        './src/js/controllers/**/*.js',
+        './src/js/services/**/*.js',
+        './src/js/pages/**/*.js'
+    ],
 
-	fonts: [
-		//vendors
-		'./bower_components/materialize/dist/font/**/*.{ttf,woff,eof,svg}'
-	]
+    css: [
+        // vendors
+        './bower_components/normalize.css/normalize.css',
+        './bower_components/materialize/dist/css/materialize.min.css'
+    ],
+
+    fonts: [
+        //vendors
+        './bower_components/materialize/dist/font/**/*.{ttf,woff,eof,svg}'
+    ]
 }
 
 /*
-* Scripts Task
-**/
+ * Scripts Task
+ **/
 gulp.task('scripts', function () {
-	gulp.src(config.scripts)
-		.pipe(plumber())
-		.pipe(concat('app.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest(config.dest + '/js'));
+    gulp.src(config.scripts)
+        .pipe(plumber())
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.dest + '/js'));
 });
 
 /*
-* CSS Task
-**/
+ * CSS Task
+ **/
 gulp.task('css', function () {
-	gulp.src(config.css)
-		.pipe(plumber())
-		.pipe(concat('app.min.css'))
-		.pipe(minifyCss())
-		.pipe(gulp.dest(config.dest + '/css'));
+    gulp.src(config.css)
+        .pipe(plumber())
+        .pipe(concat('app.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest(config.dest + '/css'));
 });
 
 /*
-* Fonts Task
-**/
-gulp.task('fonts', function() {
-	gulp.src(config.fonts)
-		.pipe(gulp.dest(config.dest + '/font'));
+ * Fonts Task
+ **/
+gulp.task('fonts', function () {
+    gulp.src(config.fonts)
+        .pipe(gulp.dest(config.dest + '/font'));
 });
 
 /*
-* Watch changes and re run tasks
-* */
+ * Watch changes and re run tasks
+ * */
 gulp.task('watch', function () {
     watch(config.css, function () {
-    	gulp.start('css');
-	});
+        gulp.start('css');
+    });
 
     watch(config.scripts, function () {
-    	gulp.start('scripts');
-	});
-    
+        gulp.start('scripts');
+    });
+
     watch(config.fonts, function () {
-    	gulp.start('fonts');
-	});
+        gulp.start('fonts');
+    });
 });
 
 /*
-* Default Task
-**/
+ * Default Task
+ **/
 gulp.task('default', ['scripts', 'css', 'fonts', 'watch']);
